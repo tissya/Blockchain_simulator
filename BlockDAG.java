13317fd4b78f874f472d6af3af2efcae0ff1857c
tissya <tissya4649753@gmail.com>
2018-03-26 13:57:45 +0900

    BlockDAGに必要そうな変数やメソッドを前もって準備
    BlockDAGのメインではない変数やメソッドを追加
    加えてMain.javaのリファクタリングをした(うまく出来てるかな・・・？)

--- BlockDAG.java
+++ BlockDAG.java
@@ -1,6 +1,11 @@
+import java.lang.Math;
+
 class BlockDAG{
-    int norma; ////全てのノードにブロックが"norma"回追加するまでシミュレートする
-    int node; //ノード数
+    private int norma; ////全てのノードにブロックが"norma"回追加するまでシミュレートする
+    private int node; //ノード数
+    private int consensus = 0;//合意されたブロック数
+    private int messageNum = 0;//送信されたメッセージ数
+    private int turn = 0;//経過ターン数
 
     //コンストラクタ
     BlockDAG(int norma,int node){
@@ -8,7 +13,23 @@ class BlockDAG{
         this.node = node;
     }
 
+    BlockDAG(){
+        //エラー
+    }
+
+    public int getMessageNum(){
+        return messageNum;
+    }
+
+    public int getTurn(){
+        return turn;
+    }
+
     public void run(){
         //実行内容を記述する
+        while(true){
+            break;
+        }
+        return;
     }
 }
--- Main.java
+++ Main.java
@@ -5,20 +5,29 @@
 class Main{
     public static void main(String[] args){
         Blockchain bc = new Blockchain(500,15); //ノルマ100, ノード数5(仮置き)
-        BlockDAG bdg = new BlockDAG(100,5); //ノルマ100, ノード数5(仮置き)
+        BlockDAG bdag = new BlockDAG(100,5); //ノルマ100, ノード数5(仮置き)
         Raft raft = new Raft(100,5); //ノルマ100, ノード数5(仮置き)
 
+        System.out.println("Simulation Start!!");
         bc.run();//Blockchainのシミュレーション開始
-        //gdg.run();//BlockDAGのシミュレーション開始
+        //gdag.run();//BlockDAGのシミュレーション開始
         raft.run();//Raftのシミュレーション開始
 
+        //Blockchainのメッセージ数とターン数を取得
         int bcMessage = bc.getMessageNum();
         int bcTurn = bc.getTurn();
+
+        //raftのメッセージ数とターン数を取得
         int raftMessage = raft.getMessageNum();
         int raftTurn = raft.getTurn();
 
-        //Blockchainシミュレート結果を出力する
+        //BlockDAGのメッセージ数とターン数を取得
+        int bdagMessage = bdag.getMessageNum();
+        int bdagTurn = bdag.getTurn();
+
+        //Blockchain,raft,BlockDAGのシミュレート結果を出力する
         System.out.println("Blockchain : Message = " + bcMessage + " Turn = " + bcTurn);
         System.out.println("Raft : Message = " + raftMessage + " Turn = " + raftTurn);
+        System.out.println("BlockDAG : Message = " + bdagMessage + " Turn = " + bdagTurn);
     }
 }
