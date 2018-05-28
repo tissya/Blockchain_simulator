36598bda88a05ace25fa3176c542a220a3d8f24a
tissya <tissya4649753@gmail.com>
2018-05-28 16:43:34 +0900

    BlockchainシミュレータのBlockchainの配信者を決める仕様を変更
    今までnode[0]が配信者としていたが、公平性を保つため、ランダムで配信者を決定するように変更した。

--- Blockchain.java
+++ Blockchain.java
@@ -9,12 +9,15 @@ Blockchainのプログラム
 
 また、このプログラムの性質として
 1,コンセンサスの数は各々のノードのチェーンに入っているブロック数を比較して最小の値を採用する
+2,配信者のノードはランダムであるが、必ず各ノードに配信の機会は訪れる
 
 このプログラムの問題点
 1,承認メッセージに返信しないのはまずい・・・？
 */
 
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Blockchain{
     int consensus = 0; //全てのノードにブロックが追加された数
@@ -23,7 +26,7 @@ class Blockchain{
     int turn = 0;//経過ターン数
     int messageNum = 0;//メッセージ数
     boolean mining = false;//マイニングのフラグ
-    boolean sendMessage = false;//マイニングしたブロック通知の管理フラグ
+    boolean anotherInspect = false;//配信者以外のノードがブロックを検査したどうかのフラグ
 
     //コンストラクタ
     Blockchain(int norma,int node){
@@ -50,38 +53,54 @@ class Blockchain{
     public void run(){
         //指定分のノードを作成
         BcNode[] node = new BcNode[nodes]; 
+        //配信ノードを決定するためのパラメータ
+        ArrayList<Integer> distributorList = new ArrayList<Integer>();
+        int distributor = 0;//配信者のノード番号
+        int sendCounter = 0;
+
         for(int i = 0; i < nodes; i++){
             node[i] = new BcNode();
+            distributorList.add(i);
         }
+        Collections.shuffle(distributorList);//Listの中身をシャッフルする
 
         while(consensus < norma){//指定ブロック数のコンセンサスが取れるまでループし続ける
             if(mining){//マイニングされた後の処理
                 //ノードNo.0が発掘したものを前提として、他のノードにブロックを検査させるようにする
-                    if(!node[0].getInspectRun() && !node[1].getInspectRun()){//ノードNo.0がブロック検査をしていない時
+                    if(!node[distributor].getInspectRun() && !anotherInspect){//ノードNo.0がブロック検査をしていない時
                         Block block = new Block();
                         sendingMessage();//自身以外にメッセージを送信する
-                        sendMessage = true;//発見したブロックを自身以外の全てのノードに通知する
-                        node[0].inspect(block);//実際はすでにinspect済みである(マイニングした時点で)
+                        node[distributor].inspect(block);//実際はすでにinspect済みである(マイニングした時点で)
                     }
 
-                    else if(node[0].getInspectRun() && !node[1].getInspectRun()){//ノードNo.0がブロック検査をしていて、かつ他のノードがブロック検査していない時
-                        node[0].addBlock();//No.0がブロックを追加
+                    else if(node[distributor].getInspectRun() && !anotherInspect){//ノードNo.0がブロック検査をしていて、かつ他のノードがブロック検査していない時
+                        node[distributor].addBlock();//No.0がブロックを追加
                         Block block = new Block();
-                        //No.0以外の全てのノードがブロック検査をする
-                        for(int i = 1; i < node.length;i++){
-                            node[i].inspect(block);
+                        //配信者以外の全てのノードがブロック検査をする
+                        for(int i = 0; i < nodes;i++){
+                            if(i != distributor) node[i].inspect(block);
                         }
+                        anotherInspect = true;
                     }
 
-                    else{//最後にNo.0以外のノード全てがブロックを追加する
-                        for(int i = 1; i < node.length;i++){
-                            node[i].addBlock();
+                    else{//最後に配信者以外のノード全てがブロックを追加する
+                        for(int i = 0; i < nodes;i++){
+                            if(i != distributor) node[i].addBlock();
                         }
                         mining = false;//全てのノードがブロックを追加した時、次のマイニングを開始する
+                        anotherInspect = false;
                     }
             }//if(mining)
 
             else{//マイニングされていない時全てのノードはマイニングをする
+                //配信者ノードをランダムで決定する
+                if(sendCounter >= nodes){
+                    sendCounter = 0;
+                    Collections.shuffle(distributorList);
+                }
+                distributor = distributorList.get(sendCounter);
+                sendCounter++;
+
                 mining = true;
             }
 
